61d419e03ea7455ea701e4a5cfa32c36cf4073e1
joao vitor ceolin prudente <joao.ceolin@origemmotos.com.br>
2023-06-03 15:37:50 -0300

    feat: git add

--- src/actions.c
+++ src/actions.c
@@ -33,7 +33,53 @@ void git_init() {
 }
 
 void git_add(char const *file_path) {
+  // make validations
+  bool is_valid = validate_repository();
+  if (!is_valid)
+    return;
+  file_exists(file_path);
 
+  // parses files.txt into repository
+  CharVector *files_txt_lines = read_lines(FILES_TXT);
+  if (files_txt_lines == NULL) {
+    printf("Error reading %s\n", FILES_TXT);
+    return;
+  }
+  Repository *repository = parse_repository(files_txt_lines);
+  free_vector(files_txt_lines);
+
+  // check if file is already added
+  Commit *on_going_commit = repository->tail_commit;
+  if (on_going_commit == NULL) {
+    printf("Open commit not found\n");
+    free_repository(repository, true, true);
+    return;
+  }
+  if (file_in_commit(on_going_commit, file_path)) {
+    printf("File already added\n");
+    free_repository(repository, true, true);
+    return;
+  }
+  free_repository(repository, false, false);
+
+  // add file to repository
+  char *file_text = malloc(128 * sizeof(char));
+  if (file_text == NULL) {
+    printf("Error allocating memory\n");
+    return;
+  }
+  sprintf(file_text, "path:%s\nstart:-1\nend:-1\n", file_path);
+  if (file_text == NULL) {
+    printf("Error allocating memory\n");
+    return;
+  }
+  int err = append_to_file(FILES_TXT, file_text);
+  if (err != 0) {
+    printf("Error appending to %s\n", FILES_TXT);
+    free(file_text);
+    return;
+  }
+  free(file_text);
 }
 
 void git_commit(char const *message) {
